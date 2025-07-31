1e571e24aa3795e2e25cee71f6f2b5739c8edce2
Luthfi <Luthfi@bangunindo.com>
2025-07-31 23:53:01 +0700

    Add test_code for testing pre-commit

---
+++ test_code.py
@@ -0,0 +1,10 @@
+def this_is_bad_format():
+    print("This function has a bad format")
+
+
+def this_is_good_format():
+    print("This function has a good format")
+
+
+this_is_bad_format()
+this_is_good_format()
